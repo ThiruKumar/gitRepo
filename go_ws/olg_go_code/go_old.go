3263dba02dbe7ef16633a671c7e75d1e85f698f3
ThiruKumar <thiru@iplon.de>
2015-12-28 16:55:45 +0530

    web programming in git

---
+++ go_ws/olg_go_code/go_old.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+type thiru struct {
+height int
+knowledge string
+}
+
+
+func main() { 
+
+/*var a *int;
+var b int = 10;
+a = &b;
+fmt.Printf("a is %d\n",*a);*/
+
+/*var a = []int{100,40,50}
+for i:=0; i<3; i++ {
+fmt.Printf("a is %d\n", a[i]);
+} */
+
+var man thiru;
+man.height = 100;
+fmt.Printf(" Thiru Height is: %d\n",man.height);
+
+
+}
+
---
+++ go_ws/olg_go_code/hello.go
@@ -0,0 +1,76 @@
+/*package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+type Page struct {
+	Title string
+	Body  []byte
+}
+
+func (p *Page) save() error {
+	filename := p.Title + ".html"
+	return ioutil.WriteFile(filename, p.Body, 0600)
+}
+
+func loadPage(title string) (*Page, error) {
+	filename := title + ".html"
+	body, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+	return &Page{Title: title, Body: body}, nil
+}
+
+func viewHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/view/"):]
+	p, _ := loadPage(title)
+	fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)
+}
+
+func main() {
+	http.HandleFunc("/view/", viewHandler)
+	http.ListenAndServe(":8080", nil)
+}*/
+
+
+
+
+/*package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+fmt.Printf("time is %d\n",time.Now())
+	for a:=1;a<11;a++ {
+	//fmt.Printf("a is %d\n",a)
+	}
+	fmt.Printf("time is %d\n",time.Now())
+}*/
+
+
+
+package main
+
+import (
+    "fmt"
+    "net/http"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+}
+
+func main() {
+    http.HandleFunc("/", handler)
+    http.ListenAndServe(":8080", nil)
+}
+
+
+
